fc97577e22829266da5b2f8dbcd255d72f25e689
Zach Machuca <zachmachuca@gmail.com>
2015-02-10 21:24:48 -0500

    removed controller metadata

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -9,19 +9,6 @@ def create
   customer = Stripe::Customer.create(
     :email => 'example@stripe.com',
     :card  => params[:stripeToken]
-    :metadata => {
-    :first_name => '', # not if company
-    :last_name => '', # not if company
-    :contact_person => '',
-    :street_line_1 => '123 Carenden Road',
-    :street_line_2 => '',
-    :city => 'London',
-    :postal_code => 'EC5M 8AJ',
-    :region => 'London',
-    :country => 'GB', # code ISO 3166-1 alpha-2
-    :tax_id => '123456789'
-  } # all metadata are optional
-)
   )
 
   charge = Stripe::Charge.create(
