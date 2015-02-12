70f3c40ebbf66874e9a0fdfa2d6f00ad451b1806
Zach Machuca <zachmachuca@gmail.com>
2015-02-12 11:05:35 -0500

    removed smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,14 +89,4 @@ Rails.application.configure do
   }
 }
 
-  config.action_mailer.delivery_method = :smtp
-  # SMTP settings for mailgun
-  ActionMailer::Base.smtp_settings = {
-    :port           => 587,
-    :address        => "smtp.mailgun.org",
-    :domain         => ENV['domain'],
-    :user_name      => ENV['username'],
-    :password       => ENV['password'],
-    :authentication => :plain,
-  }}
 end
