40abafb37cc1c0b00f39d52db0c67f726891a2c1
Zach Machuca <zachmachuca@gmail.com>
2015-02-14 15:15:38 -0500

    edited routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home', via: :get
-  resources :users, :path => "charges#create"
-  resources :charges, :path => "charges#create"
+  resources :users
+  resources :charges
 
-	match 'charges', to: 'charges#create', via: :all
 
 end
