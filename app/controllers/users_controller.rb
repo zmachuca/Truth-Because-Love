5550fa87de569ae58861d0c286b64eee33510e5c
Zach Machuca <zachmachuca@gmail.com>
2015-02-14 17:39:43 -0500

    fixed users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,23 +26,36 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      ExampleMailer.purchase_email(@user).deliver
+      ExampleMailer.sale_email(@user).deliver
     begin
-
+ 
   Rails.logger.error("STRIPE API KEY: #{Stripe.api_key.inspect}")
     @amount = 999
-
+ 
   customer = Stripe::Customer.create(
     :email => 'example@stripe.com',
     :card  => params[:stripeToken]
   )
-
+ 
   charge = Stripe::Charge.create(
     :customer    => customer.id,
     :amount      => @amount,
     :description => 'Rails Stripe Customer - TruthBecauseLove.com',
     :currency    => 'usd'
   )
-  redirect_to root_path, notice: "Your Purchase Was Successful! You should recieve an email shortly.",
+  redirect_to root_path, notice: "Your Purchase Was Successful! You should recieve an email shortly."
+  rescue Stripe::StripeError => e
+    ExampleMailer.Stripe_error_email(@user).deliver
+    flash[:error] = e.message
+    redirect_to charges_path
+  rescue Stripe::APIConnectionError => e
+    ExampleMailer.API_error_email(@user).deliver
+    flash[:error] = e.message
+    redirect_to charges_path
+  rescue => e
+    ExampleMailer.NotStripe_error_email(@user).deliver
+  end
      else
       render action: "new"
     end
