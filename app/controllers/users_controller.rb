2cf17f8a19f80949757cbcdf463ccf137a9a92d2
Zach Machuca <zachmachuca@gmail.com>
2015-02-14 21:11:07 -0500

    moved mailers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,9 +42,9 @@ class UsersController < ApplicationController
         :description => 'Rails Stripe Customer',
         :currency    => 'usd'
       )
-    redirect_to root_path, notice: "Your Purchase Was Successful! You should recieve an email shortly."
     ExampleMailer.purchase_email(@user).deliver
     ExampleMailer.sale_email(@user).deliver
+    redirect_to root_path, notice: "Your Purchase Was Successful! You should recieve an email shortly."
     rescue Stripe::StripeError => e
       ExampleMailer.Stripe_error_email(@user).deliver
       flash[:error] = e.message
